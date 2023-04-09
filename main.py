732cc8674462737066de93abe29a6afc0a7cb0c2
Mihir <mihirniyogi942@gmail.com>
2023-04-09 17:44:25 +0800

    added score text, collision detection

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Food(pygame.sprite.Sprite):
 
         self.image = loadImage('apple.png').convert_alpha()
         self.image = pygame.transform.rotozoom(self.image,0,0.6) 
-        self.rect = self.image.get_rect(center = (randrange(20,740,20),randrange(20,740,20)))
+        self.rect = self.image.get_rect(center = (randrange(20,screen_width - 20,20),randrange(20,screen_height - 20,20)))
 
 # Functions to load resources
 def loadImage(name):
@@ -30,11 +30,13 @@ def loadFont(name,size):
 # Setup - ADMIN
 pygame.init()
 pygame.display.set_caption("Snake")
-screen = pygame.display.set_mode((750,750))
+screen_width = 600
+screen_height = 600
+screen = pygame.display.set_mode((screen_width,screen_height))
 game_active = False
 titlefont = loadFont('babyblues.ttf',150)
 subtitlefont = loadFont('babyblues.ttf',30)
-
+scorefont = loadFont('babyblues.ttf',20)
 # Setup - SNAKE VARIABLES
 S_DIRECTION = "west"
 S_LENGTH = 1
@@ -43,16 +45,21 @@ S_PATH = []
 # Setup - TIMERS
 clock = pygame.time.Clock()
 snake_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(snake_timer,200)
+pygame.time.set_timer(snake_timer,130)
 
 # Sprites
 food_group = pygame.sprite.Group()
 
+# Surfaces - ACTIVe
+score_apple = loadImage('apple.png').convert_alpha()
+score_apple = pygame.transform.rotozoom(score_apple,20,0.6)
+score_apple_rect = score_apple.get_rect(center=(570,570))
+
 # Surfaces - NONACTIVE
 title = titlefont.render("snake",False,'White')
-title_rect = title.get_rect(center = (375,340))
+title_rect = title.get_rect(center = (screen_width//2,270))
 subtitle = subtitlefont.render("the retro game",False,'White')
-subtitle_rect = subtitle.get_rect(center=(375,420))
+subtitle_rect = subtitle.get_rect(center=(screen_height//2,345))
 
 # Main Loop
 while True:
@@ -87,8 +94,9 @@ while True:
         else:
             # KEY event (start / restart game)
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                start_position = (380,380)
-                S_PATH.append(start_position)
+                start_position = (screen_width//2,screen_height//2)
+                S_LENGTH = 1
+                S_PATH = [start_position]
                 S_DIRECTION = "west"
                 game_active = True
 
@@ -101,18 +109,33 @@ while True:
         # generate food if no food
         if not food_group: food_group.add(Food())
 
-        # check for collisions
-        if S_PATH[0] == food_group.sprites()[0].rect.center:
+        # collisions
+        head = S_PATH[0]
+        x,y = head
+        rect = pygame.Rect(head[0],head[1],20,20)
+        
+        # collision with food
+        if rect.colliderect(food_group.sprites()[0].rect):
             S_LENGTH += 1
             food_group.empty()
+
+        # collision with wall or itself
+        conditions = x in range(0,600) and y in range(0,600) and len(set(S_PATH)) == len(S_PATH)
+        game_active = conditions
 
         # display background
         screen.fill(("#191919"))
         food_group.draw(screen)
 
+        # display score
+        score = scorefont.render(f"Score: {S_LENGTH - 1} ",False,'White')
+        score_rect = score.get_rect(center=(530,570))
+        screen.blit(score,score_rect)
+        screen.blit(score_apple,score_apple_rect)
+
         # display snake
         for position in S_PATH:
-            snake_surface = pygame.Surface((20,20))
+            snake_surface = pygame.Surface((20,20),pygame.SRCALPHA)
             snake_rect = pygame.draw.rect(snake_surface,'#21FA90',snake_surface.get_rect(),width=0,border_radius=5)
             snake_rect.center=position
             screen.blit(snake_surface,snake_rect)
